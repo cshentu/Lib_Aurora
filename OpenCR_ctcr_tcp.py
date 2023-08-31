f53a53619fcea0a0e5c3a4f3087a1707111d4149
Chengnan <cshetnu@cs.toronto.edu>
2023-08-31 16:33:31 -0400

    add class for com with ctcr

--- OpenCR_ctcr_tcp.py
+++ OpenCR_ctcr_tcp.py
@@ -1,9 +1,22 @@
 import socket
+import numpy as np
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverAddr = ('127.0.0.1', 8083)
-clientsocket.connect(serverAddr)
-
-message = "gpos"
-clientsocket.send(message.encode())
-clientsocket.recv(1024).decode()
+class OpenCR_CTCR_tcp:
+    def __init__(self):
+        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverAddr = ('127.0.0.1', 8083)
+        self.clientsocket.connect(self.serverAddr)
+    
+    def get_joint_values(self):
+        message = "gpos"
+        self.clientsocket.send(message.encode())
+        reply = self.clientsocket.recv(1024).decode()
+        return np.fromstring(reply, dtype=float, sep=' ')
+    
+    def set_joint_values(self, target):
+        message = "spos " + np.array2string(target, separator=' ', formatter={'float_kind':lambda x: "%.3f" % x})[1:-1]
+        self.clientsocket.send(message.encode())
+        return True
+    
+ctcr = OpenCR_CTCR_tcp()
+print(ctcr.get_joint_values())
